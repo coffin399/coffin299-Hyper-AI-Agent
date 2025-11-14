26fda5b6aa9a8c20eb2bf30f61a95a21f29ff0f7
coffin299 <coffin299@ymail.ne.jp>
2025-11-14 16:31:49 +0900

    fix

---
+++ mobile/android/app/src/main/java/com/hyperagent/ReactNativeFlipper.java
@@ -0,0 +1,12 @@
+package com.hyperagent;
+
+import android.content.Context;
+import com.facebook.react.ReactInstanceManager;
+
+public final class ReactNativeFlipper {
+  private ReactNativeFlipper() {}
+
+  public static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
+    // No-op: Flipper tooling disabled in CI builds.
+  }
+}
